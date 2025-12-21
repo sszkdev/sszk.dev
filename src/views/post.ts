21b06802592b4e47581e415668c9900891afc281
sszkdev <suzuki.coding.19@gmail.com>
2025-12-21 21:32:11 +0900

    feat: simplify post rendering by removing unnecessary title variable and header

--- src/views/post.ts
+++ src/views/post.ts
@@ -3,15 +3,12 @@ import type { Post } from "../posts/posts";
 import { layout } from "./layout";
 
 export function renderPost(post: Post) {
-  const displayTitle = post.title;
-
   return layout(
-    displayTitle,
+    post.title,
     html`<div class="page">
       <main>
         <article>
-          <h1>${displayTitle}</h1>
-          <p>Published: ${post.date}</p>
+          <p>${post.date}</p>
           ${raw(post.html)}
         </article>
         <p><a href="/">Back to home</a></p>
